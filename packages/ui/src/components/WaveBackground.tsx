d2642a29eb93ad8c839e0a676b0d4dd98a0e9921
George <17267419+ps-george@users.noreply.github.com>
2024-08-03 16:39:14 +0100

    Add vars for customing Wave bg and move it down more

--- packages/ui/src/components/WaveBackground.tsx
+++ packages/ui/src/components/WaveBackground.tsx
@@ -3,22 +3,27 @@ import Svg, { Path } from "react-native-svg";
 import { View } from "tamagui";
 
 type WaveBackgroundProps = {
+  fill?: string;
+  flipX?: boolean;
   slideDown?: number;
   viewBoxHeight?: number;
   viewBoxWidth?: number;
 }
 
 export const WaveBackground = ({
-  slideDown=150, 
+  fill="#E57474",
+  flipX=false,
+  slideDown=180,
   viewBoxHeight=500,
-  viewBoxWidth=350
+  viewBoxWidth=350,
 }: WaveBackgroundProps) => {
   return (
     <View style={styles.backgroundContainer}>
       <Svg width="100%" height="100%" viewBox={`0 ${-slideDown} ${viewBoxWidth} ${viewBoxHeight}`} preserveAspectRatio="xMidYMin slice">
         <Path
-          fill="#E57474"
+          fill={fill}
           d="M167.12 77.1281C122.212 182.474 25.8197 208.81 -30.1409 208.81V594.414H423.14V23.6157C347.243 -2.44092 212.029 -28.2178 167.12 77.1281Z"
+          transform={flipX ? "scale(-1, 1) translate(-350, 0)" : ""}
         />
       </Svg>
     </View>
