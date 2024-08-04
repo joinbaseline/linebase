96bcd07d5e4dacac7fb06391d354fac25d881beb
George <17267419+ps-george@users.noreply.github.com>
2024-08-04 12:51:29 +0100

    Update version

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -6,7 +6,7 @@ const defineConfig = (): ExpoConfig => ({
   slug: 'baseline',
   jsEngine: 'hermes',
   scheme: 'baseline',
-  version: '1.0.4',
+  version: '1.0.5',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic',
